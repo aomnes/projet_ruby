e2705d7f18cbc99a331afeddf0479191628763cc
aomnes <omnes.antoine.fr@gmail.com>
2016-10-20 13:57:27 +0000

    ajout fonctionne

--- public/angular-seed/app/view1/view1.js
+++ public/angular-seed/app/view1/view1.js
@@ -2,14 +2,22 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-	.config(['$routeProvider', function($routeProvider) {
-		$routeProvider.when('/view1', {
-			templateUrl: 'view1/view1.html',
-			controller: 'View1Ctrl'
-		});
-	}])
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
 
-	.controller('View1Ctrl', ['$scope', function($scope) {
-		$scope.students = [{name: "bob", surname: "dylan"}]
 
-		}]);
+  .controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
+    $scope.students = [];
+
+    $http.get('/students.json').success(function(data){
+      $scope.students = data
+    }, function errorCallback(response) {
+
+  	});
+
+
+}]);
