ba3c2f59f5c3de9cc4b783dc37df237bb2f9a78e
aomnes <omnes.antoine.fr@gmail.com>
2016-10-23 15:40:34 +0000

    var app =;

--- public/angular-seed/app/view1/view1.js
+++ public/angular-seed/app/view1/view1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+var app = angular.module('myApp.view1', ['ngRoute']);
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
