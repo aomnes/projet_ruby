647e3753d801d2312f0854d4107666f0a2981919
antoine.omnes <antoine.omnes@utbm.fr>
2016-11-29 09:39:23 -0500

    travis token

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,10 @@
+require "simplecov"
+SimpleCov.start
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'vcr'
-
 
 VCR.configure do |config|
  config.cassette_library_dir = "fixtures/vcr_cassettes"
